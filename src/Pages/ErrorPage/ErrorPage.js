800c6740b4f852ee170e02065e5fe6d2b18b19bc
Vedant Singh <67269716+imvsr-69@users.noreply.github.com>
2021-03-23 11:17:15 +0530

    404 fage completed

--- src/Pages/ErrorPage/ErrorPage.js
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -1,10 +1,12 @@
 import "./ErrorPage.css";
+import img from '../../assets/images/img-4.svg'
 
 const ErrorPage = () => {
   return (
     <div className="error-page">
+    <img src={img} alt='404'/>
       <p className="error-msg">
-        <span>404</span>
+        Seems you're lost..
         This page could not be found.
       </p>
     </div>
---
+++ src/Pages/InstructionPage/Instruction.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Instruction.css'
+
+const Instruction = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Instruction
