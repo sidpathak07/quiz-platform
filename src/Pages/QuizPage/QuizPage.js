929c739ddf5c87a60a2a0e2ea65ecd3d34b0e89c
paramkeshkamat <keshkamatparam@gmail.com>
2021-03-24 17:13:27 +0530

    readme updated

--- src/Pages/QuizPage/QuizPage.js
+++ src/Pages/QuizPage/QuizPage.js
@@ -13,7 +13,6 @@ import FormControl from "@material-ui/core/FormControl";
 import "./QuizPage.css";
 import MathJax from "react-mathjax3";
 
-
 const getResponses = () => {
   const responses = sessionStorage.getItem("quiz-responses");
   if (responses) {
@@ -130,164 +129,181 @@ const QuizPage = () => {
         </div>
       ) : (
         <MathJax.Context
-            input='tex'
-            onLoad={ () => console.log("Loaded MathJax script!") }
-            onError={ (MathJax, error) => {
-                console.warn(error);
-                console.log("Encountered a MathJax error, re-attempting a typeset!");
-                MathJax.Hub.Queue(
-                  MathJax.Hub.Typeset()
-                );
-            } }
-            script="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js"
-            options={ {
-                messageStyle: 'none',
-                extensions: ['tex2jax.js'],
-                jax: ['input/TeX', 'output/HTML-CSS'],
-                tex2jax: {
-                    inlineMath: [['$', '$'], ['\\(', '\\)']],
-                    displayMath: [['$$', '$$'], ['\\[', '\\]']],
-                    processEscapes: true,
-                },
-                TeX: {
-                    extensions: ['AMSmath.js', 'AMSsymbols.js', 'noErrors.js', 'noUndefined.js']
-                }
-            } }>
-        <div className="quiz-page">
-          <div className="question-progress-bar">
-            <div
-              style={{ width: ((index + 1) / quiz.length) * 100 + "%" }}
-              className="question-progress"
-            ></div>
-          </div>
+          input="tex"
+          onLoad={() => console.log("Loaded MathJax script!")}
+          onError={(MathJax, error) => {
+            console.warn(error);
+            console.log(
+              "Encountered a MathJax error, re-attempting a typeset!"
+            );
+            MathJax.Hub.Queue(MathJax.Hub.Typeset());
+          }}
+          script="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js"
+          options={{
+            messageStyle: "none",
+            extensions: ["tex2jax.js"],
+            jax: ["input/TeX", "output/HTML-CSS"],
+            tex2jax: {
+              inlineMath: [
+                ["$", "$"],
+                ["\\(", "\\)"],
+              ],
+              displayMath: [
+                ["$$", "$$"],
+                ["\\[", "\\]"],
+              ],
+              processEscapes: true,
+            },
+            TeX: {
+              extensions: [
+                "AMSmath.js",
+                "AMSsymbols.js",
+                "noErrors.js",
+                "noUndefined.js",
+              ],
+            },
+          }}
+        >
+          <div className="quiz-page">
+            <div className="question-progress-bar">
+              <div
+                style={{ width: ((index + 1) / quiz.length) * 100 + "%" }}
+                className="question-progress"
+              ></div>
+            </div>
 
-          <div className="question-page-left">
-            <div className="quiz-question">
-              <h3>Question: {index + 1}</h3>
-              <div className="question-details">
-                <h2><MathJax.Html html={ quiz[index]?.question } /></h2>
-                <div className="marks-distribution">
-                  <p>Correct : {quiz[index]?.correct_marks} marks</p>
-                  <p>Incorrect : {quiz[index]?.negative_marks} marks</p>
-                  <p className="flag-question" onClick={handleFlagQuestion}>
-                    <IoFlag />
-                    {responses[index].flag
-                      ? "Unflag Question"
-                      : "Flag Question"}
-                  </p>
+            <div className="question-page-left">
+              <div className="quiz-question">
+                <h3>Question: {index + 1}</h3>
+                <div className="question-details">
+                  <h2>
+                    <MathJax.Html html={quiz[index]?.question} />
+                  </h2>
+                  <div className="marks-distribution">
+                    <p>Correct : {quiz[index]?.correct_marks} marks</p>
+                    <p>Incorrect : {quiz[index]?.negative_marks} marks</p>
+                    <p className="flag-question" onClick={handleFlagQuestion}>
+                      <IoFlag />
+                      {responses[index].flag
+                        ? "Unflag Question"
+                        : "Flag Question"}
+                    </p>
+                  </div>
                 </div>
-              </div>
 
-              <div className="quiz-options">
-                {quiz[index]?.option.length > 0 ? (
-                  <FormControl component="fieldset">
-                    <RadioGroup
-                      aria-label="gender"
+                <div className="quiz-options">
+                  {quiz[index]?.option.length > 0 ? (
+                    <FormControl component="fieldset">
+                      <RadioGroup
+                        aria-label="gender"
+                        value={responses[index]?.answer}
+                      >
+                        {quiz[index]?.option.map((option, idx) => (
+                          <FormControlLabel
+                            key={idx}
+                            value={option.key.toString()}
+                            name={option.key.toString()}
+                            control={<Radio />}
+                            label={option.option}
+                            onChange={handleResponse}
+                          />
+                        ))}
+                      </RadioGroup>
+                    </FormControl>
+                  ) : (
+                    <textarea
+                      placeholder="Type your answer here"
                       value={responses[index]?.answer}
-                    >
-                      {quiz[index]?.option.map((option, idx) => (
-                        <FormControlLabel
-                          key={idx}
-                          value={option.key.toString()}
-                          name={option.key.toString()}
-                          control={<Radio />}
-                          label={option.option}
-                          onChange={handleResponse}
-                        />
-                      ))}
-                    </RadioGroup>
-                  </FormControl>
-                ) : (
-                  <textarea
-                    placeholder="Type your answer here"
-                    value={responses[index]?.answer}
-                    onChange={handleResponse}
-                  />
+                      onChange={handleResponse}
+                    />
+                  )}
+                </div>
+              </div>
+
+              <div className="navigation-btn">
+                <button
+                  disabled={index === 0 ? true : false}
+                  onClick={handlePrevious}
+                >
+                  Previous
+                </button>
+                {index === quiz.length - 1 && (
+                  <button onClick={() => setShowSubmit(true)}>
+                    Submit test
+                  </button>
                 )}
+                <button
+                  disabled={index === quiz.length - 1 ? true : false}
+                  onClick={handleNext}
+                >
+                  Next
+                </button>
               </div>
             </div>
 
-            <div className="navigation-btn">
-              <button
-                disabled={index === 0 ? true : false}
-                onClick={handlePrevious}
-              >
-                Previous
-              </button>
-              {index === quiz.length - 1 && (
-                <button onClick={() => setShowSubmit(true)}>Submit test</button>
-              )}
-              <button
-                disabled={index === quiz.length - 1 ? true : false}
-                onClick={handleNext}
-              >
-                Next
-              </button>
-            </div>
-          </div>
+            {showSubmit && (
+              <div className="submit-confirm">
+                <div className="submit-popup">
+                  <div className="submit-pop-text">
+                    <p>
+                      <FiAlertTriangle /> Are you sure you want to submit ?
+                    </p>
+                    <p>Once you submit , All your responses will be recorded</p>
+                  </div>
 
-          {showSubmit && (
-            <div className="submit-confirm">
-              <div className="submit-popup">
-                <div className="submit-pop-text">
-                  <p>
-                    <FiAlertTriangle /> Are you sure you want to submit ?
-                  </p>
-                  <p>Once you submit , All your responses will be recorded</p>
-                </div>
-
-                <div className="confirm-button">
-                  <button onClick={() => setShowSubmit(false)}>
-                    Back to Test
-                  </button>
-                  <button onClick={handleTestSubmit}>Proceed and Submit</button>
+                  <div className="confirm-button">
+                    <button onClick={() => setShowSubmit(false)}>
+                      Back to Test
+                    </button>
+                    <button onClick={handleTestSubmit}>
+                      Proceed and Submit
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
 
-          <div className="quiz-status">
-            <CountDownTimer
-              handleTestSubmit={handleTestSubmit}
-              duration={duration}
-            />
+            <div className="quiz-status">
+              <CountDownTimer
+                handleTestSubmit={handleTestSubmit}
+                duration={duration}
+              />
 
-            <div className="quiz-navigation-stats">
-              {btnarray.map((button, idx) => {
-                return (
-                  <button
-                    key={idx}
-                    onClick={(e) => {
-                      setIndex(e.target.value - 1);
-                    }}
-                    value={button + 1}
-                    className={
-                      responses[idx].answer ? "checked-answer" : undefined
-                    }
-                    style={{ backgroundColor: responses[idx].flag && "red" }}
-                  >
-                    {button + 1}
-                  </button>
-                );
-              })}
-            </div>
-
-            <div className="choice-sign">
-              <div className="attempted-sign">
-                <button disabled={true} />
-                <p>Attempted</p>
+              <div className="quiz-navigation-stats">
+                {btnarray.map((button, idx) => {
+                  return (
+                    <button
+                      key={idx}
+                      onClick={(e) => {
+                        setIndex(e.target.value - 1);
+                      }}
+                      value={button + 1}
+                      className={
+                        responses[idx].answer ? "checked-answer" : undefined
+                      }
+                      style={{ backgroundColor: responses[idx].flag && "red" }}
+                    >
+                      {button + 1}
+                    </button>
+                  );
+                })}
               </div>
-              <div className="unattempted-sign">
-                <button disabled={true} />
-                <p>Not Attempted</p>
+
+              <div className="choice-sign">
+                <div className="attempted-sign">
+                  <button disabled={true} />
+                  <p>Attempted</p>
+                </div>
+                <div className="unattempted-sign">
+                  <button disabled={true} />
+                  <p>Not Attempted</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
         </MathJax.Context>
       )}
     </>
-     
   );
 };
 
