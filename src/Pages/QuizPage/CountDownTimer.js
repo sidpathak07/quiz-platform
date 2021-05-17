c2f91a024b9d256a4198aaa37b32e33a20622379
Vedant Singh <67269716+imvsr-69@users.noreply.github.com>
2021-05-17 12:07:13 +0530

    timer fixed

--- src/Pages/QuizPage/CountDownTimer.js
+++ src/Pages/QuizPage/CountDownTimer.js
@@ -1,23 +1,23 @@
-import { memo } from "react";
-import Countdown from "react-countdown";
-import { IoIosTimer } from "react-icons/io";
+import { memo } from 'react';
+import Countdown from 'react-countdown';
+import { IoIosTimer } from 'react-icons/io';
 
 // const getTestDuration = (timeString) =>
 //   new Date("1970-01-01T" + timeString + "Z").getTime();
 
 const CountDownTimer = ({ testSubmit, duration }) => {
-  return (
-    <div className="countdown-div">
-      <p>
-        <IoIosTimer /> Time Left :{" "}
-      </p>
-      <Countdown
-        className="quiz-countdown"
-        date={Date.now() + duration}
-        onComplete={testSubmit}
-      />
-    </div>
-  );
+	return (
+		<div className="countdown-div">
+			<p>
+				<IoIosTimer /> Time Left :{' '}
+			</p>
+			<Countdown
+				className="quiz-countdown"
+				date={Date.now() + duration}
+				onComplete={testSubmit}
+			/>
+		</div>
+	);
 };
 
 export default memo(CountDownTimer);
