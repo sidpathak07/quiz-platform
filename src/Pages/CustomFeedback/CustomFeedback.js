0aab6432f00169baceb65a3047f74fabfd2fcb75
sidpathak07 <59722193+sidpathak07@users.noreply.github.com>
2021-05-25 11:36:43 +0530

    changes in custom feedback form

--- src/Pages/CustomFeedback/CustomFeedback.js
+++ src/Pages/CustomFeedback/CustomFeedback.js
@@ -1,69 +1,105 @@
-import React from 'react';
-import NavBar from '../../Components/NavBar/NavBar';
+import React from "react";
+import NavBar from "../../Components/NavBar/NavBar";
 import { useParams, useHistory } from "react-router-dom";
-import './CustomFeedback.css';
+import "./CustomFeedback.css";
 import { useState, useEffect, useContext } from "react";
-import AddIcon from '@material-ui/icons/Add';
-import {IconButton} from "@material-ui/core";
-import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from "@material-ui/icons/Add";
+import { IconButton } from "@material-ui/core";
+import RemoveIcon from "@material-ui/icons/Remove";
 
 function CustomFeedback() {
-    let [numberOfQuestions,setNumberOfQuestions] = useState([1]);
-    const { id } = useParams();
+  let [numberOfQuestions, setNumberOfQuestions] = useState([1]);
+  const { id } = useParams();
 
-    const addelement = () => {
-        setNumberOfQuestions([...numberOfQuestions,1]); 
+  const [questions, setQuestions] = useState([]);
+  const [confirm, setConfirm] = useState(false);
+
+  let question;
+
+  const addelement = () => {
+    if (confirm) {
+      setNumberOfQuestions([...numberOfQuestions, 1]);
+      setConfirm(false);
+    } else {
+      alert("Please confirm question to add new question");
     }
+  };
 
-    const removeElement = () => {
-        let allQ = [];
-        numberOfQuestions.pop();
-        setNumberOfQuestions([...numberOfQuestions]);
-    }
+  const removeElement = (e, index) => {
+    console.log(index);
+    numberOfQuestions.pop();
+    setNumberOfQuestions([...numberOfQuestions]);
+  };
 
-    const element = 
-        (   <>
-            <div className="element">
-                <div className="p2">
-                    <label className="p4">Question</label>
-                </div>
-                <div className="p3">
-                    <input type="text" className="input1"/>
-                    <select id="responsetype" className="select">
-                        <option className="select1" value="volvo">Slider</option>
-                        <option className="select2" value="saab">Text</option>
-                        <option className="select3" value="vw">Yes/No</option>
-                    </select>
-                </div>
-                <div className="df">
-                    <div className="plus">
-                        <IconButton onClick={addelement}><AddIcon /></IconButton>
-                    </div>
-                    <div className="plus">
-                        <IconButton onClick={removeElement}><RemoveIcon /></IconButton>
-                    </div>
-                </div>
-            </div>
-            </>
-        );
-    
-    return (
-        <div className="customfeedback">
-            <div className="g1">
-                <div className="j1">
-                    <p className="title">Custom Feedback</p>
-                    <p className="p1">Add the questions for feedback.</p>
-                </div>
-                <div className="j2">
-                    <button className="btn1">Submit Feedback Questions</button>
-                </div>
-            </div>
-            
-            {numberOfQuestions.map((question,index) => {
-                return element;
-            })}
+  const handleInputChange = (e, index) => {
+    // questionArray.splice(index, 0, e.target.value);
+    // console.log(questionArray[index]);
+    question = e.target.value;
+    console.log(question);
+    // setQuestions([...questions, questionArray]);
+  };
+
+  const confirmQuestion = (e) => {
+    setQuestions([...questions, question]);
+    setConfirm(true);
+  };
+
+  const showQuestions = () => {
+    console.log(questions);
+  };
+  return (
+    <div className="customfeedback">
+      <div className="g1">
+        <div className="j1">
+          <p className="title">Custom Feedback</p>
+          <p className="p1">Add the questions for feedback.</p>
         </div>
-    )
+        <div className="j2">
+          <button className="btn1">Submit Feedback Questions</button>
+          <button onClick={showQuestions}>Show Questions</button>
+        </div>
+      </div>
+
+      {numberOfQuestions.map((question, index) => {
+        return (
+          <div id={index}>
+            <div className="element">
+              <div className="p2">
+                <label className="p4">Question</label>
+              </div>
+              <div className="p3">
+                <input
+                  type="text"
+                  className="input1"
+                  onChange={(e) => handleInputChange(e)}
+                  value={questions[index]}
+                />
+                <select id="responsetype" className="select">
+                  <option className="select1" value="volvo">
+                    Slider
+                  </option>
+                  <option className="select2" value="saab">
+                    Text
+                  </option>
+                  <option className="select3" value="vw">
+                    Yes/No
+                  </option>
+                </select>
+                <button onClick={confirmQuestion}>Confirm</button>
+              </div>
+              <div className="df">
+                <div className="plus">
+                  <IconButton onClick={addelement}>
+                    <AddIcon />
+                  </IconButton>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default CustomFeedback;
