a8adf6be18d5bea40ca9e1d79bf7d6baf8d3c91b
Yash Virani <62023226+Yashvirani@users.noreply.github.com>
2021-05-29 17:47:58 +0530

    Update PreviewFeedBack.js

--- src/Pages/CustomFeedback/PreviewFeedBack.js
+++ src/Pages/CustomFeedback/PreviewFeedBack.js
@@ -6,12 +6,73 @@ import Error from "../../Components/ErrorComponent/Error";
 import axios from "../../axios/axios";
 import { AiOutlineFileDone } from "react-icons/ai";
 import "../FeedBackPage/FeedBack.css";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+});
+
+const rating = [
+  {
+    value: 0,
+    label: '0',
+  },
+  {
+    value: 10,
+    label: '1',
+  },
+  {
+    value: 20,
+    label: '2',
+  },
+  {
+    value: 30,
+    label: '3',
+  },
+  {
+    value: 40,
+    label: '4',
+  },
+  {
+    value: 50,
+    label: '5',
+  },
+  {
+    value: 60,
+    label: '6',
+  },
+  {
+    value: 70,
+    label: '7',
+  },
+  {
+    value: 80,
+    label: '8',
+  },
+  {
+    value: 90,
+    label: '9',
+  },
+  {
+    value: 100,
+    label: '10',
+  },
+
+];
+
+  
 
 const PreviewFeedBack = () => {
   const { userDetails, userCurrentQuiz } = useContext(UserContext);
   const [questions, setQuestions] = useState([]);
   const {id} = useParams();
   const history = useHistory();
+  const classes = useStyles();
 
   const fetchQuestion = async () => {
     try {
@@ -33,7 +94,15 @@ const PreviewFeedBack = () => {
     fetchQuestion();
   },[id]);
   
+  function valuetext(value) {
+    return `${value}`;
+  }
 
+  function valueLabelFormat(value) {
+    return rating.findIndex((rating) => rating.value === value) ;
+  }
+  
+  
   return (
     <>
       <div className="top">
@@ -48,14 +117,29 @@ const PreviewFeedBack = () => {
             <div key={question.id}>
               <h3 className="student-feedback-question">{question.question}</h3>
               {question.responseType === "range" ? (
-                <input
-                  type="range"
-                  name=""
-                  id={index}
-                  max="10"
-                  min="1"
-                  className={question.id}
-                />
+                  <div className={classes.root}>
+                      <Slider
+                        defaultValue={0}
+                        valueLabelFormat={valueLabelFormat}
+                        getAriaValueText={valuetext}
+                        aria-labelledby="discrete-slider-restrict"
+                        step={null}
+                        valueLabelDisplay="auto"
+                        marks={rating}
+                        className={question.id}
+                        name=""
+                        id={index}
+                    />
+                  </div>
+                //    <input
+                //    type="range"
+                //    name=""
+                //    id={index}
+                //    max="10"
+                //    min="1"
+                //    className={question.id}
+                //  />
+            
               ) : (
                 ""
               )}
@@ -92,7 +176,12 @@ const PreviewFeedBack = () => {
                         </div>
                         
                     </div>
-                  </div>
+                    
+                  
+                  
+                  
+                  
+                </div>
               ) : (
                 ""
               )}
