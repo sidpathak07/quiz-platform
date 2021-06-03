81079c29bd1f268faa5a1223d9a48156801c14e7
sidpathak07 <59722193+sidpathak07@users.noreply.github.com>
2021-06-03 22:27:08 +0530

    Scorecard Completed.

--- src/Pages/Report/Report.js
+++ src/Pages/Report/Report.js
@@ -3,11 +3,13 @@ import "./Report.css";
 import { Chart } from "react-google-charts";
 import { useParams } from "react-router";
 import axios from "../../axios/axios";
+import Loader from "../../Components/Loader/LoadingBar";
 import UserContext from "../../Context/UserContext";
 import { useHistory } from "react-router-dom";
 function Report() {
   const { username, id } = useParams();
   console.log("USERNAME:", username, "ID:", id);
+  const [responseData, setResponseData] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const { userDetails } = useContext(UserContext);
   const history = useHistory();
@@ -15,7 +17,9 @@ function Report() {
   const [levels, setLevels] = useState([]);
   const [correctQ, setCorrectQ] = useState(0);
   const [incorrectQ, setIncorrectQ] = useState(0);
+  const [attempted, setAttempted] = useState(0);
   const [unattemptedQ, setUnattemptedQ] = useState(0);
+  const [analysis, setAnalysis] = useState({});
   const [totalQ, setTotalQ] = useState(0);
   const [accuracy, setAccuracy] = useState(0);
   const [marksObtained, setMarksObtained] = useState(0);
@@ -88,45 +92,11 @@ function Report() {
       const config = {
         headers: { Authorization: `Bearer ${userDetails.access}` },
       };
-      const postData = {
-        user: username,
-        quiz: id,
-      };
       const { data } = await axios.get(
         `/api/getresult/${username}/${id}`,
-        postData,
         config
       );
-      console.log(data);
-      const {
-        attempted,
-        correctquestion,
-        incorrectquestion,
-        marks_obtained,
-        not_attempted,
-        analysis,
-      } = data;
-      setCorrectQ(correctquestion);
-      setIncorrectQ(incorrectquestion);
-      setUnattemptedQ(not_attempted);
-      setTotalQ(attempted + not_attempted);
-      setMarksObtained(marks_obtained);
-      for (const key in analysis) {
-        // if (key.includes("subject")) {
-        //   let obj = {};
-        //   obj[key] = analysis[key];
-        //   subjects.push(obj);
-        // }
-        if (key.includes("dificulty")) {
-          let obj = {};
-          obj[key] = analysis[key];
-          lvl.push(obj);
-        }
-      }
-      setLevels(lvl);
-      let acc = ((correctQ / totalQ) * 100).toFixed(2);
-      setAccuracy(acc);
-
+      setResponseData(data);
       setIsLoading(false);
     } catch (error) {
       console.log(error);
@@ -137,113 +107,208 @@ function Report() {
     fetchResult();
   }, []);
 
+  useEffect(() => {
+    if (responseData) {
+      setChartData();
+    }
+  }, [responseData]);
+
+  const setChartData = () => {
+    setIsLoading(true);
+    console.log("Setting chart data");
+    console.log(responseData);
+    let lvl = [];
+    const {
+      attempted,
+      correctquestion,
+      incorrectquestion,
+      marks_obtained,
+      not_attempted,
+      analysis,
+    } = responseData.data;
+    console.log(
+      attempted,
+      correctquestion,
+      incorrectquestion,
+      marks_obtained,
+      not_attempted,
+      analysis
+    );
+    setAttempted(attempted);
+    setCorrectQ(correctquestion);
+    setIncorrectQ(incorrectquestion);
+    setUnattemptedQ(not_attempted);
+    setTotalQ(attempted + not_attempted);
+    setMarksObtained(marks_obtained);
+    setAnalysis(analysis);
+    for (const key in analysis) {
+      if (key.includes("dificulty")) {
+        let obj = {};
+        obj[key] = analysis[key];
+        lvl.push(obj);
+      }
+    }
+    setLevels(lvl);
+    let acc = ((correctQ / totalQ) * 100).toFixed(2);
+    setAccuracy(acc);
+    setIsLoading(false);
+  };
+
+  const showData = () => {
+    console.log(
+      "attempted",
+      attempted,
+      "CorrectQ",
+      correctQ,
+      "IncorrectQ",
+      incorrectQ,
+      "UnattemptedQ",
+      unattemptedQ,
+      "MarksObtained",
+      marksObtained
+    );
+    console.log("LEVELS:", levels);
+  };
   return (
     <div className="report">
-      <p className="head">Result Analysis - Scorecard</p>
-      <p className="time">
-        {day} , {ISTTime.getDate()} {month} {hoursIST} : {minutesIST} {ampm}
-      </p>
-      <div className="report-card">
-        <div className="nav">
-          <p
-            className="nav-item scorecard"
-            onClick={() =>
-              history.push(`/report/${userDetails.username}/${id}`)
-            }
-          >
-            Scorecard
+      {!isLoading && (
+        <>
+          <button onClick={() => showData()}>Show data</button>
+          <p className="head">Result Analysis - Scorecard</p>
+          <p className="time">
+            {day} , {ISTTime.getDate()} {month} {hoursIST} : {minutesIST} {ampm}
           </p>
-          <p
-            className="nav-item subject-report"
-            onClick={() =>
-              history.push(`/report/subjectreport/${username}/${id}`)
-            }
-          >
-            Subject Report
-          </p>
-          <p
-            className="nav-item comparative-report"
-            onClick={() =>
-              history.push(`/report/comparativereport/${username}/${id}`)
-            }
-          >
-            Comparative Report
-          </p>
-        </div>
-        <div className="rank">
-          <div className="rank-card">
-            <p className="rank-des">Rank Obtained:</p>
-            <p className="rank-rank">3000 to 4000</p>
+          <div className="report-card">
+            <div className="nav">
+              <p
+                className="nav-item scorecard"
+                onClick={() =>
+                  history.push(`/report/${userDetails.username}/${id}`)
+                }
+              >
+                Scorecard
+              </p>
+              <p
+                className="nav-item subject-report"
+                onClick={() =>
+                  history.push(`/report/subjectreport/${username}/${id}`)
+                }
+              >
+                Subject Report
+              </p>
+              <p
+                className="nav-item comparative-report"
+                onClick={() =>
+                  history.push(`/report/comparativereport/${username}/${id}`)
+                }
+              >
+                Comparative Report
+              </p>
+            </div>
+            <div className="rank">
+              <div className="rank-card">
+                <p className="rank-des">Rank Obtained:</p>
+                <p className="rank-rank">3000 to 4000</p>
+              </div>
+              <div className="marks-card">
+                <p className="rank-des">Marks Obtained:</p>
+                <p className="rank-rank">{`${marksObtained}/`}</p>
+              </div>
+            </div>
+            <div className="txt">
+              <p className="score-txt">
+                {`In this test , you have scored ${marksObtained} marks and a Rank between 3001 and
+               4000. The Test paper had a medium difficulty level. Your score in
+               the test was Okay. Other metrics of your performance on Difficulty
+               level, Subjects and the like are available below.`}{" "}
+              </p>
+            </div>
+            <div className="accuracy">
+              <p className="accuracy-1">
+                Accuracy:
+                <span
+                  style={{ color: "#214786", fontWeight: "600" }}
+                >{`${accuracy}`}</span>
+              </p>
+              <p className="attempted">
+                Total attempted questions:
+                <span style={{ color: "#214786", fontWeight: "700" }}>
+                  {`Total attempted Questions: ${
+                    correctQ + incorrectQ
+                  } of ${totalQ} (Correct:${correctQ} Incorrect:${incorrectQ})`}
+                </span>
+              </p>
+            </div>
+            <div className="graph">
+              <div className="bar-graph">
+                {!levels && (
+                  <div className="quizquestion-loader">
+                    <Loader />
+                  </div>
+                )}
+                {analysis && (
+                  <Chart
+                    width={"600px"}
+                    height={"300px"}
+                    chartType="Bar"
+                    loader={<div>Loading Chart</div>}
+                    data={[
+                      ["", "Easy", "Medium", "Hard"],
+                      [
+                        "Correct",
+                        analysis["dificulty: Easy"]?.correct_questions,
+                        analysis["dificulty: Medium"]?.correct_questions,
+                        analysis["dificulty: Hard"]?.correct_questions,
+                      ],
+                      [
+                        "Incorrect/Unattempted",
+                        analysis["dificulty: Easy"]?.incorrect_or_not_attempted,
+                        analysis["dificulty: Medium"]
+                          ?.incorrect_or_not_attempted,
+                        analysis["dificulty: Hard"]?.incorrect_or_not_attempted,
+                      ],
+                      [
+                        "Total",
+                        analysis["dificulty: Easy"]?.total_questions,
+                        analysis["dificulty: Medium"]?.total_questions,
+                        analysis["dificulty: Hard"]?.total_questions,
+                      ],
+                    ]}
+                    options={{
+                      chart: {
+                        title: "",
+                        subtitle: "",
+                      },
+                    }}
+                  />
+                )}
+              </div>
+              <div className="pie-chart">
+                <Chart
+                  width={"500px"}
+                  height={"300px"}
+                  chartType="PieChart"
+                  loader={<div>Loading Chart</div>}
+                  data={[
+                    ["Quiz", "Types of Questions"],
+                    ["Correct", correctQ],
+                    ["Incorrect", incorrectQ],
+                    ["unattempted", unattemptedQ],
+                  ]}
+                  options={{
+                    title: "Attempt Summary",
+                  }}
+                />
+              </div>
+            </div>
           </div>
-          <div className="marks-card">
-            <p className="rank-des">Marks Obtained:</p>
-            <p className="rank-rank">{`${marksObtained}/`}</p>
-          </div>
+        </>
+      )}
+      {isLoading && (
+        <div className="quizquestion-loader">
+          <Loader />
         </div>
-        <div className="txt">
-          <p className="score-txt">
-            {`In this test , you have scored ${marksObtained} marks and a Rank between 3001 and
-            4000. The Test paper had a medium difficulty level. Your score in
-            the test was Okay. Other metrics of your performance on Difficulty
-            level, Subjects and the like are available below.`}{" "}
-          </p>
-        </div>
-        <div className="accuracy">
-          <p className="accuracy-1">
-            Accuracy:
-            <span
-              style={{ color: "#214786", fontWeight: "600" }}
-            >{`${accuracy}`}</span>
-          </p>
-          <p className="attempted">
-            Total attempted questions:
-            <span style={{ color: "#214786", fontWeight: "700" }}>
-              {`Total attempted Questions: ${
-                correctQ + incorrectQ
-              } of ${totalQ} (Correct:${correctQ} Incorrect:${incorrectQ})`}
-            </span>
-          </p>
-        </div>
-        <div className="graph">
-          <div className="bar-graph">
-            <Chart
-              width={"600px"}
-              height={"300px"}
-              chartType="Bar"
-              loader={<div>Loading Chart</div>}
-              data={[
-                ["", "Easy", "Medium", "Hard"],
-                ["Correct", 10, 20, 30],
-                ["Incorrect/Unattempted", 10, 20, 30],
-                ["Total", 10, 20, 30],
-              ]}
-              options={{
-                chart: {
-                  title: "",
-                  subtitle: "",
-                },
-              }}
-            />
-          </div>
-          <div className="pie-chart">
-            <Chart
-              width={"500px"}
-              height={"300px"}
-              chartType="PieChart"
-              loader={<div>Loading Chart</div>}
-              data={[
-                ["Quiz", "Types of Questions"],
-                ["Correct", 10],
-                ["Incorrect", 5],
-                ["unattempted", 10],
-              ]}
-              options={{
-                title: "Attempt Summary",
-              }}
-            />
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
