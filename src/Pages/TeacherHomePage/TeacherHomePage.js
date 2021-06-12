2f5148465134f7745a3d4bfd3edcedbf03389b5e
Yash Virani <62023226+Yashvirani@users.noreply.github.com>
2021-06-12 15:36:51 +0530

    Update TeacherHomePage.js

--- src/Pages/TeacherHomePage/TeacherHomePage.js
+++ src/Pages/TeacherHomePage/TeacherHomePage.js
@@ -1,4 +1,4 @@
-import NavBar from "../../Components/NavBar/NavBar";
+import TeacherNavbar from "../../Components/NavBar/TeacherNavbar";
 import { useHistory } from "react-router-dom";
 import { FaEdit } from "react-icons/fa";
 import "./TeacherHomePage.css";
@@ -7,7 +7,7 @@ const TeacherHomePage = () => {
   const history = useHistory();
   return (
     <div className="teacher-homepage">
-      <NavBar />
+      <TeacherNavbar />
       <div className="homepage-card" onClick={() => history.push("/allquizzes")}>
         <div className="homepage-card-icon">
           <FaEdit />
